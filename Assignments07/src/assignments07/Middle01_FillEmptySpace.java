7daa6768c5fe1b69b9b88bab2e63fd71312b9eef
kimdaehwi990731 <167949524+kimdaehwi990731@users.noreply.github.com>
2024-05-09 09:43:23 +0900

    Update Middle01_FillEmptySpace.java

--- Assignments07/src/assignments07/Middle01_FillEmptySpace.java
+++ Assignments07/src/assignments07/Middle01_FillEmptySpace.java
@@ -16,15 +16,26 @@ public class Middle01_FillEmptySpace {
 		
 		// 남은 바이트 공간을 "a"로 채우는 반복문
 		for (int i = 0; i < remainSize; i++) {
-			if (i % 3 == 1) {
+			if (i % 3 == 0) {
 				fillEmptySpace.append("b");
 			}
-			else if (i % 3 == 2) {
+			else if (i % 3 == 1) {
 				fillEmptySpace.append("i");
 			}
-			else if (i % 3 == 0) {
+			else if (i % 3 == 2) {
 				fillEmptySpace.append("t");
 			}
+			// 혹은를 해도 된다. (남은 capacity가 25였으므로, 3으로 나누면 1부터 시작)
+		// for (int i = 0; i < remainSize; i++) {
+		// 	if ((remainSize- i) % 3 == 1) {
+		// 		fillEmptySpace.append("b");
+		// 	}
+		// 	else if ((remainSize- i) % 3 == 2) {
+		// 		fillEmptySpace.append("i");
+		// 	}
+		// 	else if ((remainSize- i) % 3 == 0) {
+		// 		fillEmptySpace.append("t");
+		// 	}
 		}
 		System.out.println("-------------------------------------------------------------");
 		System.out.println("fillEmptySpace의 remainCapacity에 'b', 'i', 't'를 돌아가며 채웁니다.");
