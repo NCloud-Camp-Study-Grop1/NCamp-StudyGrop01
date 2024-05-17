b4c552530f5a6a52a19eb3567207c11b792b3727
kimdaehwi990731 <167949524+kimdaehwi990731@users.noreply.github.com>
2024-05-17 09:53:17 +0900

    Update CombineReverse02.java

--- Assignments13/src/middle/StringBuilderUtils/CombineReverse02.java
+++ Assignments13/src/middle/StringBuilderUtils/CombineReverse02.java
@@ -3,7 +3,6 @@ package middle.StringBuilderUtils;
 import java.util.function.BiFunction;
 
 public class CombineReverse02 {
-	
 	public static void main(String[] args) {
 		// combineStrBuilder Reference Implement
 		// Function
@@ -13,22 +12,21 @@ public class CombineReverse02 {
 		// - 예시) BiFunction<매개변수의 타입, 매개변수의 타입, 반환 값의 타입>
 		BiFunction<StringBuilder, StringBuilder, StringBuilder> combineAndReverse = (sb1, sb2) ->
                 new StringBuilder(sb1).append(sb2).reverse();
+		
+		StringBuilder sb1 = new StringBuilder("짱구");
+       		StringBuilder sb2 = new StringBuilder("흰둥이");
 
-        StringBuilder sb1 = new StringBuilder("짱구");
-        StringBuilder sb2 = new StringBuilder("흰둥이");
-
-        // combRev Method
-        StringBuilder combRev = combineAndReverse.apply(sb1, sb2);
-        System.out.println("합친 후 역순으로 리턴한 값: " + combRev);
-	}
+        	// combRev Method
+       		StringBuilder combRev = combineAndReverse.apply(sb1, sb2);
+        	System.out.println("합친 후 역순으로 리턴한 값: " + combRev);
+		}
 	
-    // appendSb Method: StringBuilder 값을 합침
-    public static StringBuilder appendSb(StringBuilder sb1, StringBuilder sb2) {
-        return sb1.append(sb2);
-    }
-
-    // reverseSb Method: StringBuilder 값을 역순으로 변경
-    public static StringBuilder reverseSb(StringBuilder sb) {
-        return sb.reverse();
-    }
+    		// appendSb Method: StringBuilder 값을 합침
+    		public static StringBuilder appendSb(StringBuilder sb1, StringBuilder sb2) {
+        	return sb1.append(sb2);
+    	}
+	// reverseSb Method: StringBuilder 값을 역순으로 변경
+	public static StringBuilder reverseSb(StringBuilder sb) {
+		return sb.reverse();
+    	}
 }
